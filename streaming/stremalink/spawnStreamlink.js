6fb269916278be7aa3718132a508c926a6f1dda5
pro047 <pro047@naver.com>
2025-08-02 21:17:07 +0900

    fix stremalink option

--- streaming/stremalink/spawnStreamlink.js
+++ streaming/stremalink/spawnStreamlink.js
@@ -1,36 +1,36 @@
 require("dotenv").config();
 const { spawn } = require("child_process");
 const fs = require("fs");
-const path = require("path");
 
 const cookiePath = process.env.YOUTUBE_COOKIE_PATH;
+const streamlinkCmd = process.env.STREAMLINK_PATH || "streamlink";
 
 function parseCookiesFromFile(filePath) {
-  const cookies = [];
-
-  const lines = fs.readFileSync(filePath, "utf8").split("\n");
-
-  for (const line of lines) {
-    if (line.startsWith("#") || line.trim() === "") continue;
-
-    const parts = line.split("\t");
-    if (parts.length >= 7) {
-      const key = parts[5];
-      const value = parts[6];
-      cookies.push(`--http-cookie`);
-      cookies.push(`${key}=${value}`);
-    }
-  }
-
-  return cookies;
+  const raw = fs.readFileSync(filePath, "utf8");
+  return raw
+    .split("\n")
+    .filter((line) => /SID|HSID|SSID|SAPISID|APISID/.test(line))
+    .map((line) => {
+      const parts = line.trim().split(/\s+/);
+      return `${parts[5]}=${parts[6]}`;
+    })
+    .join(";");
 }
 
-const spawnStreamLink = (youtubeUrl) => {
-  const cookieArgs = parseCookiesFromFile(cookiePath);
+const cookieString = parseCookiesFromFile(cookiePath);
 
+const spawnStreamLink = (youtubeUrl) => {
   const streamlink = spawn(
-    "streamlink" || process.env.STREAMLINK_PATH,
-    ["-O", youtubeUrl, "best", ...cookieArgs, "--retry-streams", "999999"],
+    streamlinkCmd,
+    [
+      "--http-cookie",
+      cookieString,
+      "--retry-streams",
+      "999999",
+      "-O",
+      youtubeUrl,
+      "best",
+    ],
     { env: process.env }
   );
 
